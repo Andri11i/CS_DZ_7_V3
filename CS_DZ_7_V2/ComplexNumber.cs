2eaa3be2fab482600b8652b730ebcb551bc094eb
zotov.andrey.2008@gmail.com <zotov.andrey.2008@gmail.com>
2024-08-21 12:10:23 +0300

    Add project files.

---
+++ CS_DZ_7_V2/ComplexNumber.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_7_V2
+{
+    internal class ComplexNumber
+    {
+        public double Real { get; }
+        public double Imaginary { get; }
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+        public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+        }
+        public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
+        }
+        public static ComplexNumber operator *(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(
+                c1.Real * c2.Real - c1.Imaginary * c2.Imaginary,
+                c1.Real * c2.Imaginary + c1.Imaginary * c2.Real
+            );
+        }
+        public static ComplexNumber operator /(ComplexNumber c1, ComplexNumber c2)
+        {
+            double denominator = c2.Real * c2.Real + c2.Imaginary * c2.Imaginary;
+            return new ComplexNumber(
+                (c1.Real * c2.Real + c1.Imaginary * c2.Imaginary) / denominator,
+                (c1.Imaginary * c2.Real - c1.Real * c2.Imaginary) / denominator
+            );
+        }
+        public override string ToString()
+        {
+            return $"{Real} + {Imaginary}i";
+        }
+
+
+        public static bool operator ==(ComplexNumber c1, ComplexNumber c2)
+        {
+            return c1.Equals(c2);
+        }
+
+        public static bool operator !=(ComplexNumber c1, ComplexNumber c2)
+        {
+            return !c1.Equals(c2);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is ComplexNumber other)
+            {
+                return Real == other.Real && Imaginary == other.Imaginary;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Real, Imaginary);
+        }
+    }
+}
---
+++ CS_DZ_7_V2/Program.cs
@@ -0,0 +1,26 @@
+﻿using CS_DZ_7_V2;
+
+
+// арифметические операторы
+
+var c1 = new ComplexNumber(3, 4);
+var c2 = new ComplexNumber(1, 2);
+
+var sum = c1 + c2;
+var difference = c1 - c2;
+var product = c1 * c2;
+var quotient = c1 / c2;
+
+Console.WriteLine($"Sum: {sum}");
+Console.WriteLine($"Difference: {difference}");
+Console.WriteLine($"Product: {product}");
+Console.WriteLine($"Quotient: {quotient}");
+
+// операторы сравнения
+
+c1 = new ComplexNumber(3, 4);
+c2 = new ComplexNumber(3, 4);
+var c3 = new ComplexNumber(1, 2);
+
+Console.WriteLine($"c1 == c2: {c1 == c2}");
+Console.WriteLine($"c1 != c3: {c1 != c3}");
